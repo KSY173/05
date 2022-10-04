c85e05028d7b69642be2e179c8cf25d1b2cdad51
KSY173 <112866329+KSY173@users.noreply.github.com>
2022-10-04 17:05:06 +0900

    practice3

--- main.c
+++ main.c
@@ -4,14 +4,18 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int a;
+	char c;
+	int num = 0;
 	
-	printf("정수 하나를 입력하시오 :");
-	scanf("%d", &a);
+	printf("input a string :");
 	
-	if (a<0)
-		a = a* -1;
-	printf("절댓값은 %i 입니다.\n", a);
-
+	while ((c = getchar()) !='\n')//입력 문자가 개행문자가 나올 떄까지 반복 
+	{
+		if ( c >='0' && c <= '9')// 입력된 글자(c)가 숫자인가?
+			num = num+1;	//그렇다면 num을 하나를 센다.  
+	}
+	
+	printf("the number of digits is %i\n", num);
+	
 	return 0;
 }
