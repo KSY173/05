369453fa0a55781c29ca78d938352107c1980696
KSY173 <112866329+KSY173@users.noreply.github.com>
2022-10-04 17:19:10 +0900

    practice4

--- main.c
+++ main.c
@@ -4,18 +4,18 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char c;
-	int num = 0;
+	int sum=0;
+	int x;
 	
-	printf("input a string :");
+	printf("input a number :");
+	scanf("%d", &x);
 	
-	while ((c = getchar()) !='\n')//입력 문자가 개행문자가 나올 떄까지 반복 
-	{
-		if ( c >='0' && c <= '9')// 입력된 글자(c)가 숫자인가?
-			num = num+1;	//그렇다면 num을 하나를 센다.  
-	}
+	int i;
+	for (i=0; i<=x; i++)
+		sum = sum + i;
+		
 	
-	printf("the number of digits is %i\n", num);
+	printf("The result is %d", sum);
 	
 	return 0;
 }
